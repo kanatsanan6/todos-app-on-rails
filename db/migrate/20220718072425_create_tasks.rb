59e1e046bf6e3a70aa8848b3fa74e4cff7551d1f
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-19 13:10:51 +0700

    fix: add nullable constraint and default value

--- db/migrate/20220718072425_create_tasks.rb
+++ db/migrate/20220718072425_create_tasks.rb
@@ -3,8 +3,8 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.boolean :status
+      t.string :title, null: false, default: ''
+      t.boolean :status, null: false, default: false
 
       t.timestamps
     end
