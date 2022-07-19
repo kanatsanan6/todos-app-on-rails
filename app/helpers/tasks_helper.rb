69f07f1adb768f64d8c212a86fd30e8d6a566041
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-19 11:30:27 +0700

    feat: create landing page

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module TasksHelper
+  def progress(tasks)
+    completed_task = 0
+    tasks.each do |task|
+      completed_task += 1 if task.status
+    end
+    completed_task * 100 / tasks.length
+  end
 end
