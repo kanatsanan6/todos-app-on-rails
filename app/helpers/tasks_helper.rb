4a92f55b0f43a727179408798334cdbd5a0033f8
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-19 15:23:51 +0700

    fix: prevent divided by zero

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -6,6 +6,6 @@ module TasksHelper
     tasks.each do |task|
       completed_task += 1 if task.status
     end
-    completed_task * 100 / tasks.length
+    completed_task * 100 / (tasks.length.nonzero? || 1)
   end
 end
