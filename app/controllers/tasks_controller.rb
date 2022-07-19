de94e413d89e869758fec61824d3f43505301bc0
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-19 10:39:39 +0700

    feat: create Task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,50 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to root_url, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :status)
+  end
 end
