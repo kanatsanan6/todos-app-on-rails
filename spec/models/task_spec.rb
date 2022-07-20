1379617a0e6b48f623225f286f6455e48e3a0e72
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-20 16:38:28 +0700

    test: add enum testcases

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -14,5 +14,6 @@ RSpec.describe Task, type: :model do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :body }
     it { should validate_length_of(:title).is_at_least(5).on(:create) }
+    it { should define_enum_for(:status).with_values(in_progress: 0, done: 1) }
   end
 end
