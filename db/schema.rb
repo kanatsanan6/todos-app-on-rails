fd3cc8a37bc1e2e0df22064ca47c2f38e12180aa
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-21 17:06:58 +0700

    build: setting up devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_075729) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_100334) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'comments', force: :cascade do |t|
-    t.string 'commenter', default: '', null: false
-    t.text 'body', default: '', null: false
-    t.bigint 'task_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['task_id'], name: 'index_comments_on_task_id'
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter", default: "", null: false
+    t.text "body", default: "", null: false
+    t.bigint "task_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
   end
 
-  create_table 'tasks', force: :cascade do |t|
-    t.string 'title', default: '', null: false
-    t.integer 'status', default: 0, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.text 'body', default: '', null: false
+  create_table "tasks", force: :cascade do |t|
+    t.string "title", default: "", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "body", default: "", null: false
   end
 
-  add_foreign_key 'comments', 'tasks'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "comments", "tasks"
 end
