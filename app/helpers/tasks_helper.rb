7537149e372a9c898eab6e58313c89a35508f2d5
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-21 10:15:20 +0700

    fix: move logic from view to helper

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,8 +2,41 @@
 
 module TasksHelper
   def progress(tasks)
-    completed_task = tasks.count(&:status)
+    completed_task = tasks.count { |task| task.status == 'done' }
     all_task = tasks.length
-    completed_task * 100 / (all_task.nonzero? || 1)
+    completed_percent = completed_task * 100 / (all_task.nonzero? || 1)
+    "<p>Completed: #{completed_percent}%</p>".html_safe
+  end
+
+  def checkbox_status(task)
+    out = if task.done?
+            '<input type="checkbox" disabled checked class="text-green-600" />'
+          elsif task.in_progress?
+            '<input type="checkbox" disabled checked class="text-gray-500" />'
+          else
+            '<input type="checkbox" disabled/>'
+          end
+    out.html_safe
+  end
+
+  def title_status(task)
+    out = if task.done?
+            "<p class='line-through text-gray-400 ml-2'>#{task.title}</p>"
+          else
+            "<p class='ml-2'>#{task.title}</p>"
+          end
+    out.html_safe
+  end
+
+  def task_status(task)
+    out = 'Status: '
+    out += if task.done?
+             'Done'
+           elsif task.in_progress?
+             'In Progress'
+           else
+             'Pending'
+           end
+    out.html_safe
   end
 end
