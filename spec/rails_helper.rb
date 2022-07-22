c03936d916c94a3c5811cbd3a278c990cbc5a1df
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-22 15:46:00 +0700

    test: add/remove config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,11 +10,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 
 require 'rspec/rails'
 
-<<<<<<< HEAD
-DatabaseCleaner.strategy = :truncation
-
-=======
->>>>>>> rspec
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
