0706cf06149a6600442fe43df2d68507b7a833d0
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-22 11:19:29 +0700

    feat: add association between models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
+  belongs_to :user
   belongs_to :task
 
-  validates :commenter, presence: true
   validates :body, presence: true
 end
