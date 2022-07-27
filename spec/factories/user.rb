ab73cdcbaf39016beda687b781b1054318d89c6a
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-07-27 17:42:35 +0700

    test: add test for avatar uploader

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "test-#{n}@sample.com" }
     password { '123456' }
+    avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/images/example.png')) }
   end
 end
