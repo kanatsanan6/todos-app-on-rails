50386d2b2a55e73c22615b4b4be4b3685b1fad92
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-08-02 18:59:19 +0700

    format: reformat worker

--- app/workers/schedule/every_morning_worker.rb
+++ app/workers/schedule/every_morning_worker.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
-class Schedule::EveryMorningWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: :mailer, retry: 0
+module Schedule
+  class EveryMorningWorker
+    include Sidekiq::Worker
 
-  def perform
-    User.find_each do |user|
-      TaskMailer.with(email: user.email).reminder_email.deliver_later
+    sidekiq_options queue: :schedule, retry: 0
+
+    def perform
+      User.find_each do |user|
+        TaskMailer.with(email: user.email).reminder_email.deliver_later
+      end
     end
   end
 end
