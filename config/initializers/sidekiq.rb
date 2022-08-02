3248b485a09eef75eff8c6196127039c7f254eb4
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-08-02 19:00:39 +0700

    feat: add worker schedule

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'sidekiq/middleware/i18n'
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', nil) }
 end
@@ -7,3 +11,6 @@ end
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', nil) }
 end
+
+schedule_file = 'config/schedule.yml'
+Sidekiq::Cron::Job.load_from_hash(YAML.load_file(schedule_file)) if File.exist?(schedule_file) && Sidekiq.server?
