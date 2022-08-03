5b534bd7b24a8434f888ca2dfd7d3ca03e2006dc
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-08-03 12:47:02 +0700

    test: add test for worker and mailer

--- spec/mailers/task_spec.rb
+++ spec/mailers/task_spec.rb
@@ -3,5 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe TaskMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'checks task mailer' do
+    let!(:task) { create(:task) }
+    let!(:mail) { TaskMailer.with(email: 'test@email.com', tasks: Task.all.as_json).reminder_email }
+
+    it 'renders the correct subject' do
+      expect(mail.subject).to eq('A reminder from todos-on-rails!')
+    end
+
+    it 'renders the correct receiver' do
+      expect(mail.to).to match_array ['test@email.com']
+    end
+
+    it 'renders the correct sender' do
+      expect(mail.from).to match_array ['railstestingemail1@gmail.com']
+    end
+  end
 end
---
+++ spec/workers/every_morning_worker_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'fugit'
+
+RSpec.describe Schedule::EveryMorningWorker, type: :worker do
+  let(:time) { (Time.zone.today + 6.hours).to_datetime }
+  let(:scheduled_job) { described_class.perform_at(time) }
+
+  describe 'testing worker' do
+    it 'is enqueued in the schedul queue' do
+      described_class.perform_async
+
+      assert_equal :schedule, described_class.queue
+    end
+
+    it 'goes into jobs array for testing environment' do
+      expect { described_class.perform_async }.to change(described_class.jobs, :size).by(1)
+    end
+  end
+
+  describe 'testing scheduled tasks' do
+    schedule_file = Rails.root.join('config/schedule.yml')
+    schedule = YAML.load_file(schedule_file)
+
+    context 'check cron syntax' do
+      schedule.each do |cron_name, cron_info|
+        cron = cron_info['cron']
+
+        it "#{cron_name} has correct cron syntax" do
+          expect { Fugit.do_parse(cron) }.not_to raise_error
+        end
+      end
+    end
+
+    context 'check scheduled tasks' do
+      it 'has a correct setting and occured at given time' do
+        scheduled_job
+        job = described_class.jobs.last
+
+        expect(job['jid'].include?(scheduled_job)).to eq true
+        expect(job['queue']).to eq 'schedule'
+        expect(job['class']).to eq described_class.to_s
+        expect(job['retry']).to eq 0
+        expect(Time.zone.at(job['at'])).to eq time
+      end
+    end
+  end
+end
