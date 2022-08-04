a92776ba67d6d14c3865989531cd24b2853672fa
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-08-04 13:04:15 +0700

    feat: assign defualt role to new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_create :assign_default_role
+
+  def assign_default_role
+    add_role(:user) if roles.blank?
+  end
+
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
 end
