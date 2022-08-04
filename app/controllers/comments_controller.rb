5bbaecca4037aa3d4b7166068cbe5426161f2a7b
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-08-04 17:42:56 +0700

    fix: delete un-used action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -43,10 +43,6 @@ class CommentsController < ApplicationController
     @comment = @task.comments.find(params[:id])
   end
 
-  def check_user
-    redirect_to task_path(@task) and return unless @comment.user_id == current_user.id
-  end
-
   def comment_params
     params.require(:comment).permit(:body).merge(user: current_user)
   end
