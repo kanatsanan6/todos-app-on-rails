a46c8e511e747adf30ce1a7034ad453ab843688a
kanatsanan6 <kanatsanan.j1998@gmail.com>
2022-08-04 14:54:36 +0700

    format: refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:avatar, :username, { role_ids: [] })
+    params.require(:user).permit(:avatar, :username, role_ids: [])
   end
 end
